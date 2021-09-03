dbea556bd1186ebe7b63671bb765964db83cfee8
Philip Perez Castro <jperez.c92@gmail.com>
2021-09-03 03:27:32 -0500

    CREATED - attendance table

---
+++ database/migrations/2021_09_03_082558_attendance.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Attendance extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attendance', function (Blueprint $table) {
+            $table->id();
+            $table->date("date");
+            $table->timestamps();
+            $table->unsignedBigInteger('gradeId');
+            $table->foreign('gradeId')->references('id')->on('grade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attendance');
+    }
+}
