0f3e7695a872de3a9deb953d8a67ba7bb95a9297
Philip Perez Castro <jperez.c92@gmail.com>
2021-09-03 03:25:15 -0500

    CREATED - assignedScore table

--- database/migrations/2021_09_03_082005_assigned_score.php
+++ database/migrations/2021_09_03_082005_assigned_score.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class Activity extends Migration
+class AssignedScore extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,14 @@ class Activity extends Migration
      */
     public function up()
     {
-        Schema::create('activity', function (Blueprint $table) {
+        Schema::create('assignededScore', function (Blueprint $table) {
             $table->bigIncrements("id");
-            $table->string("name", 100);
-            $table->integer("value");
+            $table->integer('value');
             $table->timestamps();
-            $table->unsignedBigInteger('gradeId');
-            $table->foreign('gradeId')->references('id')->on('grade');
+            $table->unsignedBigInteger('scoreId');
+            $table->foreign('scoreId')->references('id')->on('score');
+            $table->unsignedBigInteger('studentId');
+            $table->foreign('studentId')->references('id')->on('student');
         });
     }
 
@@ -30,6 +31,6 @@ class Activity extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('activity');
+        Schema::dropIfExists('assignededScore');
     }
 }
