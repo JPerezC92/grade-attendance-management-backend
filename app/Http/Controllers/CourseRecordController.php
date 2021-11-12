c82fd5f6c66ca1da7fa138c34cb25956b1c0805f
Philip Perez Castro <jperez.c92@gmail.com>
2021-11-12 01:11:03 -0500

    UPDATED - CourseRecordController order by created_at

--- app/Http/Controllers/CourseRecordController.php
+++ app/Http/Controllers/CourseRecordController.php
@@ -146,6 +146,7 @@ class CourseRecordController extends Controller
                     "attendanceCheck.*",
                     DB::raw("(SELECT attendanceStatus.value FROM attendanceStatus WHERE attendanceStatus.id = attendanceCheck.attendanceStatusId) as attendanceStatusValue")
                 )
+                ->orderBy("created_at")
                 ->get();
 
             $scoresAssigned = ScoreAssigned::whereIn("scoreAssigned.studentId", $studentsIds)
@@ -259,6 +260,7 @@ class CourseRecordController extends Controller
                     "attendanceCheck.*",
                     DB::raw("(SELECT attendanceStatus.value FROM attendanceStatus WHERE attendanceStatus.id = attendanceCheck.attendanceStatusId) as attendanceStatusValue")
                 )
+                ->orderBy("created_at")
                 ->get();
 
             $scoresAssigned = ScoreAssigned::whereIn("scoreAssigned.studentId", $studentsIds)
